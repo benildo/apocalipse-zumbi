67e796a24e9da115eb6f749551ad0c2bfa131798
Benildo <benildo@gmail.com>
2019-06-13 23:08:01 -0300

    Adicionando colisão

---
+++ Assets/Scripts/ControlaCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaCamera : MonoBehaviour
+
+{
+    public GameObject Jogador;
+    Vector3 distanciaCompensada;
+    // Start is called before the first frame update
+    void Start()
+    {
+        distanciaCompensada = transform.position - Jogador.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Jogador.transform.position + distanciaCompensada;
+    }
+}
