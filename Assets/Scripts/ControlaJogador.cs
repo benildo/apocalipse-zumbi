64dc90ad384704c8e6d5bacb3efdacd529eea19a
Benildo <benildo@gmail.com>
2019-06-17 08:00:23 -0300

    Adicionando controle inimigos

--- Assets/Scripts/ControlaJogador.cs
+++ Assets/Scripts/ControlaJogador.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class ControlaJogador : MonoBehaviour
 {
     public float Velocidade = 10;
+    Vector3 direcao;
       // Update is called once per frame
     void Update()
     {
         float eixoX = Input.GetAxis("Horizontal");
         float eixoZ = Input.GetAxis("Vertical");
 
-        Vector3 direcao = new Vector3(eixoX, 0, eixoZ);
+        direcao = new Vector3(eixoX, 0, eixoZ);
 
-        transform.Translate(direcao * Velocidade * Time.deltaTime);
+
         if (direcao != Vector3.zero)
         {
             GetComponent<Animator>().SetBool("Movendo", true);
@@ -23,6 +24,11 @@ public class ControlaJogador : MonoBehaviour
             GetComponent<Animator>().SetBool("Movendo", false);
 
         }
+
+    }
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(transform.position + (direcao * Velocidade * Time.deltaTime));
 
     }
 }
