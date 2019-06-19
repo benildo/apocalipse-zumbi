444f4cf91d0230237ade782549060f7ac96e09c0
Benildo <benildo@gmail.com>
2019-06-18 23:33:01 -0300

    Adicionando tiro

---
+++ Assets/Scripts/Bala.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+
+    public float Velocidade = 15;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition
+            (GetComponent<Rigidbody>().position +
+            transform.forward * Velocidade * Time.deltaTime);
+    }
+}
--- Assets/Scripts/ControlaArma.cs
+++ Assets/Scripts/ControlaArma.cs
@@ -15,6 +15,9 @@ public class ControlaArma : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(Bala, CanoArma.transform.position, CanoArma.transform.rotation);
+        }
     }
 }
